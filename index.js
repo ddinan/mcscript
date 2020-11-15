7e3a62923ba13e42c991d2b2dea25aca55d071db
VenkSociety <VenkSociety@users.noreply.github.com>
2020-11-15 12:55:41 +1000

    Store server configurations in config.ini

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 
 const { createServer } = require('./client.js')
+const fs = require('fs')
+const ini = require('ini')
+
+const config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'))
 
 createServer({
-  'port': process.env.PORT,
-  'name': 'MCScript Server',
-  'motd': 'Server made in JavaScript!',
-  'max-players': 20,
-  'public': true,
-  'online-mode': false,
-  'disable-op-command': false,
-  'ops': [ 'Venk', 'Waxtz' ],
+  'port': config.general.port,
+  'name': config.general.name,
+  'motd': config.general.motd,
+  'max-players': config.general.maxPlayers,
+  'public': config.public,
+  'online-mode': config.online,
+  'disable-op-command': config.disableOp,
+  'ops': config.ops,
   'plugins': { cosmetics: {} }
 })
